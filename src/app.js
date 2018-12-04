04704873a2631ac7957f8854bc39d3efa2e6483f
Lienren <lienren@vip.qq.com>
2018-12-04 12:14:01 +0800

    清除content-encoding请求头编码

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 /*
- * @Author: Lienren 
- * @Date: 2018-04-19 11:52:42 
+ * @Author: Lienren
+ * @Date: 2018-04-19 11:52:42
  * @Last Modified by: Lienren
- * @Last Modified time: 2018-06-20 18:30:21
+ * @Last Modified time: 2018-12-04 12:13:12
  */
 'use strict';
 
@@ -22,6 +22,12 @@ app.use(koastatic(path.resolve(__dirname, config.sys.staticPath)));
 
 // 配置跨域访问
 app.use(cors());
+
+// 清除content-encoding请求头编码
+app.use(async (ctx, next) => {
+  delete ctx.request.headers['content-encoding'];
+  await next();
+});
 
 // 使用koa-bodyparser中间件
 app.use(
