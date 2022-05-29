f579ee010e1dc23b90afef11940a8494b0c21583
Lienren <lienren@vip.qq.com>
2022-05-29 14:30:16 +0800

    update

--- src/controllers/samp/rearend.js
+++ src/controllers/samp/rearend.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Lienren
  * @Date: 2021-09-04 22:52:54
- * @LastEditTime: 2022-05-25 11:08:36
+ * @LastEditTime: 2022-05-29 14:16:19
  * @LastEditors: Lienren
  * @Description: 
  * @FilePath: /node-templete/src/controllers/samp/rearend.js
@@ -859,67 +859,20 @@ module.exports = {
     }
   },
   exportUsers: async ctx => {
-    let { tradeTypes, postNames, depName1s, depName2s, depStreet, name, phone, idcard, tradeType, postName, periodType, street, community, streets, communitys, address, userType,
-      sampStartTime, sampName, sampUserName, sampHandleTime, createTime, updateTime } = ctx.request.body;
+    let { batch_no, name, tel, cert_no, createTime, updateTime, cusId, cusName, opStatus, opStatusName, connectType, connectTypes, depName } = ctx.request.body;
 
     let where = {};
 
-    Object.assign(where, depStreet && { depStreet })
+    Object.assign(where, batch_no && { batch_no })
     Object.assign(where, name && { name })
-    Object.assign(where, phone && { phone })
-    Object.assign(where, idcard && { idcard })
-    Object.assign(where, tradeType && { tradeType })
-    Object.assign(where, postName && { postName })
-    Object.assign(where, periodType && { periodType })
-    Object.assign(where, userType && { userType })
-    Object.assign(where, street && { street })
-    Object.assign(where, community && { community })
-    Object.assign(where, sampName && { sampName })
-    Object.assign(where, sampUserName && { sampUserName })
-
-    if (tradeTypes && tradeTypes.length > 0) {
-      where.tradeType = {
-        $in: tradeTypes
-      }
-    }
-
-    if (postNames && postNames.length > 0) {
-      where.postName = {
-        $in: postNames
-      }
-    }
-
-    if (depName1s && depName1s.length > 0) {
-      where.depName1 = {
-        $in: depName1s
-      }
-    }
-
-    if (depName2s && depName2s.length > 0) {
-      where.depName2 = {
-        $in: depName2s
-      }
-    }
-
-    if (streets && streets.length > 0) {
-      where.street = {
-        $in: streets
-      }
-    }
-
-    if (communitys && communitys.length > 0) {
-      where.community = {
-        $in: communitys
-      }
-    }
-
-    if (sampStartTime && sampStartTime.length > 0) {
-      where.sampStartTime = { $between: sampStartTime }
-    }
-
-    if (sampHandleTime && sampHandleTime.length > 0) {
-      where.sampHandleTime = { $between: sampHandleTime }
-    }
+    Object.assign(where, tel && { tel })
+    Object.assign(where, cert_no && { cert_no })
+    Object.assign(where, cusId && { cusId })
+    Object.assign(where, cusName && { cusName })
+    Object.assign(where, opStatus && { opStatus })
+    Object.assign(where, opStatusName && { opStatusName })
+    Object.assign(where, connectType && { connectType })
+    Object.assign(where, depName && { areaName: depName })
 
     if (createTime && createTime.length > 0) {
       where.createTime = { $between: createTime }
@@ -929,243 +882,94 @@ module.exports = {
       where.updateTime = { $between: updateTime }
     }
 
-    if (address) {
-      where.address = {
-        $like: `%"${address}"%`
+    if (connectTypes && connectTypes.length > 0) {
+      where.connectType = {
+        $in: connectTypes
       }
     }
 
     let users = await ctx.orm().info_users.findAll({
-      where
+      where,
+      order: [
+        ['id', 'desc']
+      ]
     });
-
-    let samps = []
-    if (users && users.length > 0) {
-      let where1 = '';
-
-      if (tradeTypes && tradeTypes.length > 0) {
-        where1 += ' and u.tradeType in (' + tradeTypes.map(m => {
-          return `'${m}'`
-        }).join(',') + ')';
-      }
-
-      if (postNames && postNames.length > 0) {
-        where1 += ' and u.postName in (' + postNames.map(m => {
-          return `'${m}'`
-        }).join(',') + ')';
-      }
-
-      if (depName1s && depName1s.length > 0) {
-        where1 += ' and u.depName1 in (' + depName1s.map(m => {
-          return `'${m}'`
-        }).join(',') + ')';
-      }
-
-      if (depName2s && depName2s.length > 0) {
-        where1 += ' and u.depName2 in (' + depName2s.map(m => {
-          return `'${m}'`
-        }).join(',') + ')';
-      }
-
-      if (depStreet) {
-        where1 += ` and u.depStreet = '${depStreet}'`;
-      }
-
-      if (name) {
-        where1 += ` and u.name = '${name}'`;
-      }
-
-      if (phone) {
-        where1 += ` and u.phone = '${phone}'`;
-      }
-
-      if (idcard) {
-        where1 += ` and u.idcard = '${idcard}'`;
-      }
-
-      if (tradeType) {
-        where1 += ` and u.tradeType = '${tradeType}'`;
-      }
-
-      if (postName) {
-        where1 += ` and u.postName = '${postName}'`;
-      }
-
-      if (periodType) {
-        where1 += ` and u.periodType = '${periodType}'`;
-      }
-
-      if (street) {
-        where1 += ` and u.street = '${street}'`;
-      }
-
-      if (community) {
-        where1 += ` and u.community = '${community}'`;
-      }
-
-      if (streets && streets.length > 0) {
-        where1 += ' and u.street in (' + streets.map(m => {
-          return `'${m}'`
-        }).join(',') + ')';
-      }
-
-      if (communitys && communitys.length > 0) {
-        where1 += ' and u.community in (' + communitys.map(m => {
-          return `'${m}'`
-        }).join(',') + ')';
-      }
-
-      if (address) {
-        where1 += ` and u.address like '%${address}%'`;
-      }
-
-      if (userType) {
-        where1 += ` and u.userType = '${userType}'`;
-      }
-
-      if (sampStartTime && sampStartTime.length > 0) {
-        where1 += ` and u.sampStartTime between '${sampStartTime[0]}' and '${sampStartTime[1]}'`;
-      }
-
-      if (sampName) {
-        where1 += ` and u.sampName = '${sampName}'`;
-      }
-
-      if (sampUserName) {
-        where1 += ` and u.sampUserName = '${sampUserName}'`;
-      }
-
-      if (sampHandleTime && sampHandleTime.length > 0) {
-        where1 += ` and u.sampHandleTime between '${sampHandleTime[0]}' and '${sampHandleTime[1]}'`;
-      }
-
-      if (createTime && createTime.length > 0) {
-        where1 += ` and u.createTime between '${createTime[0]}' and '${createTime[1]}'`;
-      }
-
-      if (updateTime && updateTime.length > 0) {
-        where1 += ` and u.updateTime between '${updateTime[0]}' and '${updateTime[1]}'`;
-      }
-
-      let sql1 = `select s.*, u.depName1, u.depName2, u.depStreet, u.name, u.phone, u.idcard, u.street, u.community, u.address from info_user_samps s 
-      inner join info_users u on u.id = s.userId 
-      where 1=1 ${where1}`;
-      samps = await ctx.orm().query(sql1);
-    }
 
     let xlsxObj = [];
     xlsxObj.push({
-      name: '采样人员列表',
-      data: []
-    })
-
-    xlsxObj.push({
-      name: '采样信息列表',
+      name: '回访数据',
       data: []
     })
 
     xlsxObj[0].data.push([
-      '编号',
-      '部门',
-      '单位',
-      '单位所在街道',
-      '姓名',
-      '手机号',
-      '身份证号',
-      '行业类别',
-      '职业名称',
-      '采样周期',
-      '街道',
-      '社区',
-      '住址',
-      '用户类型',
-      '采集开始时间',
-      '最新采样点名称',
-      '最新采样人姓名',
-      '最新采样时间',
-      '创建时间',
-      '最后修改时间'
+      '序号',
+      '回访日期',
+      '话务员姓名',
+      '服务对象',
+      '联系电话',
+      '身份证',
+      '年龄',
+      '性别',
+      '现居住地址',
+      '服务地址',
+      '服务项目',
+      '服务人员',
+      '服务组织',
+      '开始时间',
+      '结束时间',
+      '服务时长(分钟)',
+      '现居住县区市',
+      '是否为本人',
+      '是否享受上门服务',
+      '享受服务项目',
+      '服务次数/月',
+      '服务时长/次',
+      '服务人员是否固定',
+      '服务人员是否着工作服、戴工牌',
+      '服务员是否留下联系方式',
+      '是否清楚提供服务的组织名称',
+      '对服务人员整体评价',
+      '服务意见和建议',
+      '备注',
+      '总结'
     ])
-
-    xlsxObj[1].data.push([
-      '编号',
-      '部门',
-      '单位',
-      '单位所在街道',
-      '姓名',
-      '手机号',
-      '身份证号',
-      '职业名称',
-      '采样周期',
-      '街道',
-      '社区',
-      '住址',
-      '应采样开始时间',
-      '应采样结束时间',
-      '采样状态',
-      '实际采样时间',
-      '采样点',
-      '采样人',
-      '创建时间',
-      '最后修改时间'
-    ])
-
 
     for (let i = 0, j = users.length; i < j; i++) {
       let user = users[i];
 
       let arr = new Array();
-      arr.push(user.id || '');
-      arr.push(user.depName1 || '');
-      arr.push(user.depName2 || '');
-      arr.push(user.depStreet || '');
+      arr.push(user.id);
+      arr.push(user.cusTime);
+      arr.push(user.cusName);
       arr.push(user.name);
-      arr.push(user.phone);
-      arr.push(user.idcard);
-      arr.push(user.tradeType);
-      arr.push(user.postName);
-      arr.push(user.periodType);
-      arr.push(user.street);
-      arr.push(user.community);
+      arr.push(user.tel);
+      arr.push(user.cert_no);
+      arr.push(user.age);
+      arr.push(user.sex);
       arr.push(user.address);
-      arr.push(user.userType);
-      arr.push(user.sampStartTime);
-      arr.push(user.sampName);
-      arr.push(user.sampUserName);
-      arr.push(user.sampHandleTime);
-      arr.push(user.createTime);
-      arr.push(user.updateTime);
+      arr.push(user.aap0112);
+      arr.push(user.cag0104);
+      arr.push(user.bbp0103);
+      arr.push(user.bae0104);
+      arr.push(user.cbp0107);
+      arr.push(user.cbp0108);
+      arr.push(user.cbp0113);
+      arr.push(user.addr_area);
+      arr.push(user.qa1);
+      arr.push(user.qa2);
+      arr.push(user.qa3);
+      arr.push(user.qa4);
+      arr.push(user.qa5);
+      arr.push(user.qa6);
+      arr.push(user.qa7);
+      arr.push(user.qa8);
+      arr.push(user.qa9);
+      arr.push(user.qa10);
+      arr.push(user.qa11);
+      arr.push(user.qa12);
+      arr.push(user.qa13);
 
       xlsxObj[0].data.push(arr)
-    }
-
-    for (let i = 0, j = samps.length; i < j; i++) {
-      let samp = samps[i];
-      // let user = users.find(f => f.id === samp.dataValues.userId)
-
-      let arr = new Array();
-      arr.push(samp.id || '');
-      arr.push(samp.depName1 || '');
-      arr.push(samp.depName2 || '');
-      arr.push(samp.depStreet || '');
-      arr.push(samp.name);
-      arr.push(samp.phone);
-      arr.push(samp.idcard);
-      arr.push(samp.postName);
-      arr.push(samp.periodType);
-      arr.push(samp.street);
-      arr.push(samp.community);
-      arr.push(samp.address);
-      arr.push(samp.startTime);
-      arr.push(samp.endTime);
-      arr.push(samp.handleType);
-      arr.push(samp.handleTime);
-      arr.push(samp.sampName);
-      arr.push(samp.sampUserName);
-      arr.push(samp.createTime);
-      arr.push(samp.updateTime);
-
-      xlsxObj[1].data.push(arr)
     }
 
     let excelFile = await excel.exportBigMoreSheetExcel(xlsxObj)
