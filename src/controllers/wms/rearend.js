28f57808f4c2603f9d9d6fec6f86b397d418e5d7
Lienren <lienren@vip.qq.com>
2024-10-24 21:21:21 +0800

    update

--- src/controllers/wms/rearend.js
+++ src/controllers/wms/rearend.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Lienren
  * @Date: 2021-09-04 22:52:54
- * @LastEditTime: 2024-10-20 11:56:14
+ * @LastEditTime: 2024-10-23 21:32:48
  * @LastEditors: Lienren
  * @Description: 
  * @FilePath: /node-templete/src/controllers/wms/rearend.js
@@ -1048,7 +1048,7 @@ module.exports = {
           // 拣货中或已出库订单不进入出库单中
           let sql = `select p.id, p.order_code, p.pro_code from info_outwh_pro p 
           inner join info_outwh o on o.id = p.o_id 
-          where p.order_code = '${order_code}' and p.pro_code = '${pro_code}' and o.o_status in ('拣货中', '已出库') and o.is_del = 0 limit 1`
+          where p.order_code = '${order_code}' and o.o_status in ('拣货中', '已出库') and o.is_del = 0 limit 1`
           let result = await ctx.orm().query(sql)
           if (result && result.length > 0) {
             // 如果存在，则更新
@@ -1074,7 +1074,7 @@ module.exports = {
             pro_unit: data[i][8],
             pro_num: parseInt(data[i][10]),
             pro_out_status: '库存充足',
-            o_source: '美菜',
+            o_source: '美菜订单',
             line_name: data[i][7],
             account_name: data[i][21],
             sales_price: data[i][9],
@@ -1097,7 +1097,7 @@ module.exports = {
           // 拣货中或已出库订单不进入出库单中
           let sql = `select p.id, p.order_code, p.pro_code from info_outwh_pro p 
           inner join info_outwh o on o.id = p.o_id 
-          where p.order_code = '${order_code}' and p.pro_code = '${pro_code}' and o.o_status in ('拣货中', '已出库') and o.is_del = 0 limit 1`
+          where p.order_code = '${order_code}' and o.o_status in ('拣货中', '已出库') and o.is_del = 0 limit 1`
           let result = await ctx.orm().query(sql)
           if (result && result.length > 0) {
             // 如果存在，则更新
